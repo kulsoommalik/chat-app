094eb7e28ed1a2546c985060bfc77634f9e71fd6
Kulsoom <m.kulsoom22@gmail.com>
2019-01-28 16:36:56 +0500

    newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();   //initiating request
+socket.on('connect', function (){
+    console.log('Connected to server');
+
+    // socket.emit('createEmail', {
+    //     to: 'blahblah@gamil.com',
+    //     text:'something text'
+    // });
+    socket.emit('createMessage', {  //emiting create message event
+        form: 'asad',
+        text: 'hey!!!'
+    });
+
+});
+
+socket.on('disconnect', function (){
+    console.log('Disconnected from server');
+});
+
+//custom event
+// socket.on('newEmail', function (email){  //listenser for email
+//     console.log('Email: ', email);
+// });
+socket.on('newMessage', function (message){
+    console.log('New Message: ', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,47 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 
 var app = express();  
+var server = http.createServer(app);    //express at backend uses http thats why passing directly app
+var io = socketIO(server);              //connection btw client and server
+
+
 app.use(express.static(publicPath));    //configuration
 
-app.listen(port, ()=>{                  //listenser
+io.on('connection', (socket)=>{   //registering listener to connection args-> event name, callback
+    console.log('new user connected');
+
+    // socket.emit('newEmail', {    //args-> event name, data
+    //     from: 'kiki@example.com',
+    //     text: 'some message',
+    //     createdAt: 123
+    // });
+    // socket.on('createEmail', (newEmail)=>{
+    //     console.log('create email: ', newEmail);
+    // });
+
+    socket.on('createMessage', (message)=>{ //listenser for create message event
+        console.log('Create Message: ', message);
+    });
+
+    socket.emit('newMessage', {
+        from: 'Kiki',
+        text: 'Hello world',
+        createdAt: 123
+    });
+
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(port, ()=>{                  //listenser
     console.log('Server up on port: ' + port);
 });
